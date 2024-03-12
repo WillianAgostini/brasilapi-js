a39cd121909f0cc1f2c8eb356de6079099d52646
Larner Diogo <31572276+larnerdoj@users.noreply.github.com>
2024-03-12 18:19:22 -0300

    Update cep.ts

--- src/routes/cep.ts
+++ src/routes/cep.ts
@@ -5,6 +5,8 @@ import { onlyNumbers } from "../utils/format";
 const endpointV1 = "/cep/v1/";
 const endpointV2 = "/cep/v2/";
 
-export const getBy = (cep: string, version: string = 'v2') => {
-    (version === "v2") ? get<CepV2>(endpointV2 + onlyNumbers(cep)) : get<CepV1>(endpointV1 + onlyNumbers(cep));
+export const getBy = (cep: string, version: string = "v2") => {
+  return version === "v2"
+    ? get<CepV2>(endpointV2 + onlyNumbers(cep))
+    : get<CepV1>(endpointV1 + onlyNumbers(cep));
 };
