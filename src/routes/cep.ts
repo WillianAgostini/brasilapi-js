fac4f99d8b8237526ddabc20bb7fa9727b26eb63
Larner Diogo <31572276+larnerdoj@users.noreply.github.com>
2024-03-12 17:52:00 -0300

    Update cep.ts

--- src/routes/cep.ts
+++ src/routes/cep.ts
@@ -1,16 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getByV1 = void 0;
-exports.getByV2 = void 0;
-const api_service_1 = require("../service/api.service");
-const format_1 = require("../utils/format");
+import { get } from "../service/api.service";
+import { CepV1, CepV2 } from "../types/cep";
+import { onlyNumbers } from "../utils/format";
+
 const endpointV1 = "/cep/v1/";
 const endpointV2 = "/cep/v2/";
-const getByV1 = (cep) => {
-    return (0, api_service_1.get)(endpointV1 + (0, format_1.onlyNumbers)(cep));
+
+export const getBy = (cep: string, version: string = 'v2') => {
+    (version === "v2") ? get<CepV2>(endpointV2 + onlyNumbers(cep)) : get<CepV1>(endpointV1 + onlyNumbers(cep));
 };
-const getByV2 = (cep) => {
-    return (0, api_service_1.get)(endpointV2 + (0, format_1.onlyNumbers)(cep));
-};
-exports.getByV1 = getByV1;
-exports.getByV2 = getByV2;
